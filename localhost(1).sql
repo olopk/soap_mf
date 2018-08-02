23751bf0925796ba4229ef8d18c0fd9fa3a638fe
olopk <olekwojas@gmail.com>
2018-08-02 20:13:50 +0200

    dodalem if/else dla dodawania lub updateowania kontrahentow ktorzy juz sa w bazie aplikacja

---
+++ localhost(1).sql
@@ -0,0 +1,125 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Czas generowania: 02 Sie 2018, 20:10
+-- Wersja serwera: 10.1.28-MariaDB
+-- Wersja PHP: 7.1.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `aplikacja`
+--
+CREATE DATABASE IF NOT EXISTS `aplikacja` DEFAULT CHARACTER SET utf8 COLLATE utf8_polish_ci;
+USE `aplikacja`;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `kontrahent_status`
+--
+
+CREATE TABLE `kontrahent_status` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nazwa` varchar(50) COLLATE utf8_polish_ci NOT NULL,
+  `nip` bigint(10) NOT NULL,
+  `kod` varchar(5) COLLATE utf8_polish_ci NOT NULL,
+  `komunikat` varchar(100) COLLATE utf8_polish_ci NOT NULL,
+  `data_utworzenia` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `data_aktualizacji` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `kontrahent_status`
+--
+
+INSERT INTO `kontrahent_status` (`id`, `nazwa`, `nip`, `kod`, `komunikat`, `data_utworzenia`, `data_aktualizacji`) VALUES
+(12, 'OpenIT', 8431614194, 'C', 'Podmiot o podanym identyfikatorze podatkowym NIP jest zarejestrowany jako podatnik VAT czynny', '2018-08-02 15:43:12', '0000-00-00 00:00:00'),
+(13, 'Firma Krzak', 843000000, 'I', 'Niepoprawny Numer Identyfikacji Podatkowej', '2018-08-02 15:43:12', '0000-00-00 00:00:00'),
+(14, 'Przedsiebiorstwo komunalne', 8430003548, 'C', 'Podmiot o podanym identyfikatorze podatkowym NIP jest zarejestrowany jako podatnik VAT czynny', '2018-08-02 15:43:12', '0000-00-00 00:00:00'),
+(15, 'Test', 999111666, 'I', 'Niepoprawny Numer Identyfikacji Podatkowej', '2018-08-02 17:52:06', '0000-00-00 00:00:00'),
+(16, 'Costam ktostam', 222444555, 'I', 'Niepoprawny Numer Identyfikacji Podatkowej', '2018-08-02 17:53:55', '2018-08-02 17:57:52');
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indexes for table `kontrahent_status`
+--
+ALTER TABLE `kontrahent_status`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `kontrahent_status`
+--
+ALTER TABLE `kontrahent_status`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+--
+-- Baza danych: `subiekt`
+--
+CREATE DATABASE IF NOT EXISTS `subiekt` DEFAULT CHARACTER SET utf8 COLLATE utf8_polish_ci;
+USE `subiekt`;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `kontrahenci`
+--
+
+CREATE TABLE `kontrahenci` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nazwa` varchar(50) COLLATE utf8_polish_ci NOT NULL,
+  `nip` bigint(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `kontrahenci`
+--
+
+INSERT INTO `kontrahenci` (`id`, `nazwa`, `nip`) VALUES
+(1, 'OpenIT', 8431614194),
+(2, 'Firma Krzak', 843000000),
+(3, 'Przedsiebiorstwo komunalne', 8430003548),
+(4, 'Test', 999111666),
+(5, 'Costam ktostam', 222444555);
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indexes for table `kontrahenci`
+--
+ALTER TABLE `kontrahenci`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `kontrahenci`
+--
+ALTER TABLE `kontrahenci`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
