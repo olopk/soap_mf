0bee3c839fab39e4e180756fb6e9be4b4ff718f4
olopk <olekwojas@gmail.com>
2018-08-18 13:36:23 +0200

    napisalem funkcje ktora bedzie sprawdzac czy mamy zapisane credentials w bazie, i jesli tak to uzyje ich do pobrania danych uzytkownika

--- engine.php
+++ engine.php
@@ -1,30 +1,84 @@
 <?php
 
+//$dbdriver = 'mysql';
+//$servername = 'localhost';
+//$dbname = 'subiekt';
+//$username = 'root';
+//$password = '';
+//$col_nip = 'nip';
+//$col_contractor = 'nazwa';
+//$wsdl = 'http://localhost/soap/wsdl.xml';
+
+$dbdriver = '';
+$servername = '';
+$dbname = '';
+$username = '';
+$password = '';
+$col_nip = '';
+$col_contractor = '';
+$wsdl = 'http://localhost/soap/wsdl.xml';
+
+
+$testowa = "cos tam";
+echo $testowa;
+
+credentialsLoad();
+echo $testowa;
 function credentialsLoad(){
-    
+    //this function will check if there are the credentials to collect the data from the user DB, if there are the app will run correctly.
     try{
         $conn = new PDO("mysql:host=localhost;dbname=aplikacja", 'root', '');
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $cred = $conn->query("SELECT * from db_credentials")->fetchAll();
+        
+        global $dbdriver;
+        global $servername;
+        global $username;
+        global $password;
+        global $dbname;
+        global $col_nip;
+        global $col_contractor;
+        
+        if(count($cred)>0){
+                    $dbdriver = $cred[0]['dbdriver'];
+                    $servername = $cred[0]['servername'];
+                    $username = $cred[0]['dblogin'];
+                    $password = $cred[0]['dbpass'];
+                    $dbname = $cred[0]['dbname'];
+                    $col_nip = $cred[0]['col_nip'];
+                    $col_contractor = $cred[0]['col_kon'];
+            return true;
+        }
+        else{
+            return false;
+        }
+   
+        
     }
     catch(PDOException $e){
         echo "Cannot load the credentials. Error code: " . $e->getMessage();
+        return false;
     
     }
 }
 
 function load(){
     
+        global $dbdriver;
+        global $servername;
+        global $username;
+        global $password;
+        global $dbname;
+        global $col_nip;
+        global $col_contractor;
+        global $wsdl;
         $records = '';
         $records_checked = '';
-
-        $servername = 'localhost';
-        $username = 'root';
-        $password = '';
-        $wsdl = 'http://localhost/soap/wsdl.xml';
+        
         //connection to the Subiect DB to retrieve the contractors data.
 
         try{
-            $conn = new PDO("mysql:host=$servername;dbname=subdiekt", $username, $password);
+            $conn = new PDO("$dbdriver:host=$servername;dbname=$dbname", $username, $password);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  
             $records = $conn->query('SELECT * FROM kontrahenci');     
         }
@@ -39,18 +93,18 @@ function load(){
             $client = new SoapClient($wsdl);
             //loop through the all of contractors and check their status.
             foreach($records as $record){
-                $response = $client->__soapCall("SprawdzNIP", array($record['nip']));            
+                $response = $client->__soapCall("SprawdzNIP", array($record[$col_nip]));            
                 $array = json_decode(json_encode($response), True);
                 // now we check if the contractor already exists in the db, if it is we only update his status.
-                $c = "SELECT * FROM kontrahent_status WHERE nip='".$record['nip']."';";
+                $c = "SELECT * FROM kontrahent_status WHERE nip='".$record[$col_nip]."';";
                 $check = $conn->query($c);
 
                 if(!$check->rowCount() > 0){    
-                    $insert = "INSERT INTO kontrahent_status (nazwa, nip, kod, komunikat) VALUES ('".$record['nazwa']."', '".$record['nip']."', '".$array['Kod']."', '".$array['Komunikat']."')";  
+                    $insert = "INSERT INTO kontrahent_status (nazwa, nip, kod, komunikat) VALUES ('".$record[$col_contractor]."', '".$record[$col_nip]."', '".$array['Kod']."', '".$array['Komunikat']."')";  
                     $conn->query($insert);
                 }
                 else{
-                    $update = "UPDATE kontrahent_status SET nazwa ='".$record['nazwa']."', kod = '".$array['Kod']."', komunikat = '".$array['Komunikat']."' WHERE nip='".$record['nip']."';";
+                    $update = "UPDATE kontrahent_status SET nazwa ='".$record[$col_contractor]."', kod = '".$array['Kod']."', komunikat = '".$array['Komunikat']."' WHERE nip='".$record[$col_nip]."';";
                     $conn->query($update);
                 }
             }
@@ -82,9 +136,12 @@ function credentialsSave($driver, $servername, $login, $pass, $dbname, $tbname, 
         $i.="INSERT INTO db_credentials (dbdriver, servername, dblogin, dbpass, dbname, tbname, col_nip, col_kon) VALUES ('".$driver."', '".$servername."', '".$login."', '".$pass."', '".$dbname."', '".$tbname."', '".$col_nip."', '".$col_contractor."')";
         //run the query.
         $insert = $conn->query($i); 
+        
+        return true;
     }
     catch(PDOException $e){
         echo "cos nie bangla : " . $e->getMessage();
+        return false;
     }
 }
 
